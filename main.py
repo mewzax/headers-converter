2aaa939d685be60c1d700914076317f78e08e811
slow <lilian.filoche2007@gmail.com>
2022-05-09 19:00:53 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-headers = open("headers.txt", "r+").read().splitlines()
-file = open("file.txt", "r+")
+headers = open('headers.txt', 'r').read().splitlines()
+output = open('output.txt', "w")
+
+output.write("headers = {\n")
 
 for header in headers:
-    header2 = "\"" + header.split(":")[0] + "\"" + ": " + "\"" + header.split(":")[1].strip() + "\""
-    file.write(header2 + "\n")
+  header = "\t\""+header.split(":")[0]+"\""+": "+"\""+header.split(":")[1].strip()+"\",\n"
+  output.write(header)
+  
+output.write('}')
+output.close()
